48a066a8f16985e26b658b1c962a23748d49f2d8
dw.wizard <dw.wizard@gmail.com>
2025-12-15 20:27:34 -0600

    Specify IP addr via command line args.

--- test/testPythonPlotting.py
+++ test/testPythonPlotting.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')) # Up 1 directory
@@ -7,8 +8,12 @@ import time
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--ip', default="127.0.0.1", help="The IPv4 address to send plot messages to.")
+    args = parser.parse_args()
+
     sp.createFlushThread(100)
-    sp.networkConfigure("x.x.x.x", 2000)
+    sp.networkConfigure(args.ip, 2000)
 
     for i in range(100):
         sp.plotList_1D(range(i), 1e6, -1, "python", "num")
